52a8643e246f470065e3c7f5d658a982919966de
Hafeya Khokhar <hafeyakhokhar33@gmail.com>
2026-04-02 17:11:51 -0400

    Updated Article.java

--- src/main/java/edu/upenn/cit5940/common/dto/Article.java
+++ src/main/java/edu/upenn/cit5940/common/dto/Article.java
@@ -1,5 +1,36 @@
 package edu.upenn.cit5940.common.dto;
 
 public class Article {
+	private String id;
+    private String title;
+    private String content;
+    private String date;
 
+    public Article(String id, String title, String content, String date) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+        this.date = date;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    @Override
+    public String toString() {
+        return "Article\n" + "ID: " + id + "\n" + "Title: " + title + "\n" + "Date: " + date;
+    }
 }
